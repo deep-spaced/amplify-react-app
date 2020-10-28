5d3eb637b5a11db9a9eace345413a438f8dbcb18
Andrew Anderson <aanderson@sequoiainc.com>
2020-10-28 16:11:01 -0400

    Auto deploy

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>This is a test to see if auto deploy works. </p>
         <a
           className="App-link"
           href="https://reactjs.org"
